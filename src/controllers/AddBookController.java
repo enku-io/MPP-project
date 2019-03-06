bb4d834bf2c3d5fccb81515c3f571e26af9f4f08
enku2015 <enku.wende@gmail.com>
2019-03-06 09:47:02 -0600

    Changes

--- src/controllers/AddBookController.java
+++ src/controllers/AddBookController.java
@@ -1,14 +1,22 @@
 package controllers;
 
+import dataaccess.Address;
 import dataaccess.Book;
 import dataaccess.BookAuthor;
+import dataaccess.BookCopy;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import util.Storage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -24,39 +32,52 @@ public class AddBookController {
     @FXML
     private TextField authorBio;
     @FXML
-    private TextField numberOfCopies;
-    @FXML
     private Button addBook;
     @FXML
     private Label errorLabel;
 
     @FXML
     private void addBookEvent(ActionEvent event){
-        if(isbn.getText().isEmpty() || title.getText().isEmpty() || authorFirstName.getText().isEmpty() || authorLastName.getText().isEmpty() || authorBio.getText().isEmpty() || numberOfCopies.getText().isEmpty()){
+
+        if(isbn.getText().isEmpty() || title.getText().isEmpty() || authorFirstName.getText().isEmpty() || authorLastName.getText().isEmpty() || authorBio.getText().isEmpty()){
             if(isbn.getText().isEmpty()){
-                errorLabel.setText(errorLabel.getText() + " ISBN is required");
+                errorLabel.setText( errorLabel.getText() + " ISBN is required\n");
             }
             if(title.getText().isEmpty()){
-                errorLabel.setText(errorLabel.getText() + " title is required");
+                errorLabel.setText( errorLabel.getText() + " title is required\n");
             }
             if(authorFirstName.getText().isEmpty()){
-                errorLabel.setText(errorLabel.getText() + " authorFirstName is required");
+                errorLabel.setText( errorLabel.getText() + " authorFirstName is required\n");
             }
             if(authorLastName.getText().isEmpty()){
-                errorLabel.setText(errorLabel.getText() + " authorLastName is required");
+                errorLabel.setText(errorLabel.getText() + " authorLastName is required\n");
             }
             if(authorBio.getText().isEmpty()){
-                errorLabel.setText(errorLabel.getText() + " authorBio is required");
-            }
-            if(numberOfCopies.getText().isEmpty()){
-                errorLabel.setText(errorLabel.getText() + " numberOfCopies is required");
+                errorLabel.setText(errorLabel.getText() + " authorBio is required\n");
             }
         }
         else{
-            ArrayList<BookAuthor> authors = new ArrayList<BookAuthor>();
-            authors.add(new BookAuthor(authorFirstName.getText().trim(),authorLastName.getText().trim(),null,null,authorBio.getText(),null));
-            Book book = new Book(Integer.parseInt(isbn.getText().trim()),title.getText().trim(),authors,null);
-            Storage.session.getPersonRole().addBooks(book);
+            try {
+                ArrayList<BookAuthor> authors = new ArrayList<BookAuthor>();
+                authors.add(new BookAuthor(authorFirstName.getText().trim(),authorLastName.getText().trim(),"1-641-451-7053","asdf",authorBio.getText(),new Address("asdf","asdf","asdf","asdf")));
+                ArrayList<BookCopy> copy  = new ArrayList<BookCopy>();
+                Book book = new Book(Integer.parseInt(isbn.getText().trim()),title.getText().trim(),authors,copy);
+                Storage.session.getPersonRole().addBooks(book);
+                Parent root;
+                root = FXMLLoader.load(getClass().getClassLoader().getResource("views/admin.fxml"));
+                Stage stage = new Stage();
+                stage.setTitle("Library System");
+                stage.setScene(new Scene(root, 450, 450));
+
+                stage.show();
+                ((Node)(event.getSource())).getScene().getWindow().hide();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            catch(NumberFormatException e){
+                errorLabel.setText(errorLabel.getText() + " ISBN and Number of Copies should be Numbers");
+            }
         }
 
     }
--- src/controllers/LibrarianController.java
+++ src/controllers/LibrarianController.java
@@ -1,4 +1,62 @@
 package controllers;
 
+import dataaccess.view.BookView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import util.DataConversion;
+import util.Storage;
+
+import java.io.IOException;
+
 public class LibrarianController {
+    @FXML
+    private TableView tableView;
+    @FXML
+    private Button checkoutButton;
+    @FXML
+    TableColumn isbnColumn;
+    @FXML
+    TableColumn titleColumn;
+    @FXML
+    TableColumn authorColumn;
+    @FXML
+    TableColumn isAvailableColumn;
+    @FXML
+    TableColumn copiesColumn;
+
+    @FXML
+    public void initialize(){
+        isbnColumn.setCellValueFactory(new PropertyValueFactory<BookView,String>("isbn"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<BookView,String>("title"));
+        authorColumn.setCellValueFactory(new PropertyValueFactory<BookView,String>("author"));
+        isAvailableColumn.setCellValueFactory(new PropertyValueFactory<BookView,String>("isAvailable"));
+        copiesColumn.setCellValueFactory(new PropertyValueFactory<BookView,Number>("copies"));
+        tableView.setItems(DataConversion.getBookListView(Storage.session));
+    }
+
+    @FXML
+    private void checkoutEvent(ActionEvent event){
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("views/checkout_book.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Checkout Book");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
