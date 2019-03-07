687900a5ce2ad73fcfb964652696e97d0e69416d
enku2015 <enku.wende@gmail.com>
2019-03-07 10:36:03 -0600

    Bug Fix

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("views/login.fxml"));
         primaryStage.setTitle("Hello World");
         Scene scene = new Scene(root, 795.0, 488.0);
-        scene.getStylesheets().add("./src/DarkTheme.css");
+        scene.getStylesheets().add("DarkTheme.css");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
